9724f1addcdd41771db9e9a6419d5940aade2abc
Simon Hawkenson <7358924+sihawken@users.noreply.github.com>
2025-12-22 23:38:05 -0800

    small change

--- build_files/scripts/01-cachyos_kernel_and_ublue_akmods.sh
+++ build_files/scripts/01-cachyos_kernel_and_ublue_akmods.sh
@@ -32,19 +32,19 @@ KERNEL=$(dnf5 list kernel-cachyos -q | awk '/kernel-cachyos/ {print $2}' | head 
 
 dnf5 -y copr enable ublue-os/akmods
 
-dnf install -y akmod-framework-laptop-*.fc"${RELEASE}"."${ARCH}" || \
+dnf5 install -y akmod-framework-laptop-*.fc"${RELEASE}"."${ARCH}" || \
 akmods --force --kernels "${KERNEL}" --kmod framework-laptop
 
-dnf install -y akmod-openrazer-*.fc"${RELEASE}"."${ARCH}" || \
+dnf5 install -y akmod-openrazer-*.fc"${RELEASE}"."${ARCH}" || \
 akmods --force --kernels "${KERNEL}" --kmod openrazer
 
-dnf install -y akmod-v4l2loopback-*.fc"${RELEASE}"."${ARCH}" || \
+dnf5 install -y akmod-v4l2loopback-*.fc"${RELEASE}"."${ARCH}" || \
 akmods --force --kernels "${KERNEL}" --kmod v4l2loopback
 
-dnf install -y akmod-wl-*.fc"${RELEASE}"."${ARCH}" || \
+dnf5 install -y akmod-wl-*.fc"${RELEASE}"."${ARCH}" || \
 akmods --force --kernels "${KERNEL}" --kmod wl
 
-dnf install -y akmod-xone-*.fc"${RELEASE}"."${ARCH}" || \
+dnf5 install -y akmod-xone-*.fc"${RELEASE}"."${ARCH}" || \
 akmods --force --kernels "${KERNEL}" --kmod xone
 
 dnf5 -y copr disable ublue-os/akmods
